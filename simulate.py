48667511db126d9fab6726f1ea1ff658841fd1ae
Tamme Wollweber <wittetam@mpsd-hpc-gpu-005.desy.de>
2023-10-05 15:12:37 +0200

    Code to divide spectrum in 2 halves/modes in comments

--- simulate.py
+++ simulate.py
@@ -26,13 +26,15 @@ class Signal():
         print('det_shape: ', self.det_shape)        
         self.binning = binning
         self.lines = lines
-        self.offset = self.det_shape[0]//2 - self.det_shape[0]//4 
-        self.kscale_x = 2*np.pi/self.det_shape[0]
+        #self.offset = self.det_shape[0]//2 - self.det_shape[0]//4 
+        self.offset = self.det_shape[0]//2
+        self.kscale_x = 2*np.pi/(self.det_shape[0])
         #self.kscale_y = 2*np.pi/self.det_shape[1]
         self.num_modes = num_modes
 
-        self.size_emitter = 10
+        self.size_emitter = 32
         self.sample = None
+        self.center = np.array(self.det_shape)//2 - 1
         self.hits = []
         self.hit_size = None
         self.shots = shots 
@@ -81,9 +83,10 @@ class Signal():
             self.sample[70:80, 30:80] = 1
         else:
             half_size = int(self.size_emitter/2)
-            self.sample[(self.offset+25-half_size):(self.offset+25+half_size),(self.offset+40-half_size):(self.offset+40+half_size)] = 1
+            self.sample[(self.offset-half_size):(self.offset+half_size),(self.offset-half_size):(self.offset+half_size)] = 1
             
         print('sample shape: ', len(np.nonzero(self.sample)[0]))
+        self.sample = ndi.gaussian_filter(sig.sample, 1)
         #2D
         #indices = np.nonzero(self.sample)
         #self.num_scatterer = len(indices[0])  # number of scatterers
@@ -100,13 +103,11 @@ class Signal():
         #self.r_k = np.dot(self.loc_scatterer,self.kvector)
  
         # 1D
-        self.loc_scatterer = np.nonzero(self.sample)[0]
-        self.num_scatterer = len(self.loc_scatterer)  # number of scatterers
+        self.loc_scatterer = np.nonzero(self.sample)[0] - self.center[0]
         self.kvector = np.arange(self.det_shape[0])
         self.kvector -= self.det_shape[0]//2
         self.kvector = self.kvector * self.kscale_x
 
-        self.r_k = np.matmul(self.loc_scatterer[:,np.newaxis],self.kvector[np.newaxis,:])
 
        
     def worker(self, i, counter, mean_counts, dict_raw):
@@ -115,26 +116,57 @@ class Signal():
         #print('\r', counter + i)
         np.random.seed(i+int(time.time()))
         diff_pattern = np.zeros(self.det_shape)
+        """
+        Calculate spectrum including Kbeta, Kalpha1 and Kalpha2. Detecter energy range 8keV-9keV
+        """
+        spectrum = lorentzian(np.arange(self.det_shape[1]), 845/self.binning, 0.2, 3.7/self.binning) +  lorentzian(np.arange(self.det_shape[1]), 194.7/self.binning, 1, 2.11/self.binning) + lorentzian(np.arange(self.det_shape[1]), 179/self.binning, 0.5, 2.17/self.binning)  
+        dist = np.random.poisson(spectrum/spectrum.sum()*0.9*mean_counts/self.num_modes, self.det_shape[1])
+        #argmin = np.argmin(spectrum)
+        #num_scatterer = (dist[:argmin].sum(), dist[argmin:].sum())
+        num_scatterer = dist.sum()
         if not self.incoherent:
-            phases_rand = np.zeros((self.num_scatterer,1))
-        indices = np.arange(self.num_scatterer)
-        np.random.shuffle(indices)
+            phases_rand = np.zeros((np.sum(num_scatterer),1))
+       
+        res = np.zeros(self.det_shape)
         for m in range(self.num_modes):
+            # Kalpha and Kbeta do not interfer:
+            indices = np.arange(dist.sum())
+            np.random.shuffle(indices)
+            #indices = (indices[:num_scatterer[0]], indices[num_scatterer[0]:])
+            #spec_mode = np.zeros(self.det_shape)
             if self.incoherent:
-                phases_rand = np.array(np.random.random(size=(self.num_scatterer//self.num_modes,self.det_shape[1]))*2*np.pi)
-            psi = np.exp(1j*(self.r_k[indices[(m*(self.num_scatterer//self.num_modes)):((m+1)*(self.num_scatterer//self.num_modes))],:,np.newaxis].transpose(1,0,2)+phases_rand)).sum(1).reshape(self.det_shape)
-            mode_intensity = np.multiply(np.conjugate(psi), psi)
-            norm = np.sum(mode_intensity,axis=0)
-            """
-            Calculate spectrum including Kbeta, Kalpha1 and Kalpha2. Detecter energy range 8keV-9keV
-            """
-            spectrum = lorentzian(np.arange(self.det_shape[1]), 845/self.binning, 0.2, 3.7/self.binning) +  lorentzian(np.arange(self.det_shape[1]), 194.7/self.binning, 1, 2.11/self.binning) + lorentzian(np.arange(self.det_shape[1]), 179/self.binning, 0.5, 2.17/self.binning)  
-            dist = np.random.poisson(spectrum/spectrum.sum()*mean_counts/self.num_modes, self.det_shape[1])
-            int_norm = np.divide(mode_intensity,norm)*dist
-            int_filter = ndi.gaussian_filter(int_norm, sigma=(0,1.13), mode='constant')
-            int_p = np.random.poisson(np.abs(int_filter),size=int_norm.shape)
+                phases_rand = np.array(np.random.random(size=(num_scatterer//self.num_modes,self.det_shape[1]))*2*np.pi)
+            r_k = np.matmul(self.loc_scatterer[indices%len(self.loc_scatterer),np.newaxis],self.kvector[np.newaxis,:])
+            psi = np.exp(1j*(r_k[m*(num_scatterer//self.num_modes):(m+1)*(num_scatterer//self.num_modes),:,np.newaxis].transpose(1,0,2)+phases_rand)).sum(1).reshape(self.det_shape)
+            mode_int = np.abs(psi)**2
+            norm = np.sum(mode_int,axis=0)
+            int_scaled = mode_int/norm * dist
+            #filter due to energy uncertainty
+            int_filter = ndi.gaussian_filter(int_scaled, sigma=(0,0.2), mode='constant')
+            #filter due to estimated coherence across columns
+            #int_filter = ndi.gaussian_filter(int_scaled, sigma=(0,3), mode='constant')
+
+            int_p = np.random.poisson(int_filter,size=int_filter.shape)
             int_p *= self.adu_phot
             diff_pattern += int_p
+                                      
+            #for p in range(2):
+                #phases_rand = np.array(np.random.random(size=(num_scatterer[p]//self.num_modes,self.det_shape[1]))*2*np.pi)
+                #r_k = np.matmul(self.loc_scatterer[indices[p],np.newaxis],self.kvector[np.newaxis,:])
+                #psi = np.exp(1j*(r_k[m*(num_scatterer[p]//self.num_modes):(m+1)*(num_scatterer[p]//self.num_modes),:].transpose(1,0)+phases_rand)).sum(1).reshape(self.det_shape[0])
+                #mode_int = np.abs(psi)**2
+                #norm = np.sum(mode_int,axis=0)
+                
+                #if p == 0:
+                #    spec_mode[:,:argmin] = (mode_int/norm)[:, np.newaxis] * dist[:argmin]
+                #else:
+                #spec_mode[:,argmin:] = (mode_int/norm)[:, np.newaxis] * dist[argmin:]
+                #int_norm = (mode_intensity / norm) * dist
+                #int_filter = ndi.gaussian_filter(spec_mode, sigma=(0,1.13), mode='constant')
+                #int_norm = int_filter * dist
+                #int_p = np.random.poisson(np.abs(int_norm),size=int_norm.shape)
+                #int_p *= self.adu_phot
+                #diff_pattern += int_p
         
         gauss_noise = np.random.normal(self.noise_level,2.5,self.det_shape)
         diff_pattern += gauss_noise
