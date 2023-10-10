fix q scaling issue

gpu_sim.py
@@ -36,6 +36,7 @@ class Signal():
         self.lines = lines
         self.offset = self.det_shape[0]//2
         self.kscale_x = 2*np.pi/(self.det_shape[0]*4)
+        self.ind_scale = 1000
         self.efilter = efilter
         self.num_modes = num_modes
         self.alpha_modes = alpha_modes
@@ -161,14 +162,14 @@ class Signal():
         diff_pattern = cp.zeros(self.det_shape)
         if self.alpha_modes == 1:
             num_scatterer = self.num_photons
-            indices = cp.tile(cp.arange(self.num_photons), self.num_modes).reshape(self.num_modes, self.num_photons)[:, :self.num_photons//self.num_modes]
-
-            cp.random.shuffle(indices.T)
+            indices = cp.random.randint(0, self.num_photons, size=(self.num_modes, self.num_photons//self.num_modes)) % self.ind_scale
+            #indices = cp.random.random((self.num_modes, self.num_photons//self.num_modes))
+            #indices = ((indices-indices.min())*(self.loc_scatterer.max()-self.loc_scatterer.min())) / (indices.max()-indices.min()) - self.loc_scatterer.min()
             if self.incoherent:
                 phases_rand = cp.array(cp.random.random(size=(self.num_modes, num_scatterer//self.num_modes))*2*cp.pi)
             else:
-                phases_rand = cp.zeros(self.num_modes, num_scatterer//self.num_modes)
-            r_k = cp.matmul(self.loc_scatterer[indices%len(self.loc_scatterer),cp.newaxis],self.kvector[cp.newaxis,:])
+                phases_rand = cp.zeros((self.num_modes, num_scatterer//self.num_modes))
+            r_k = cp.matmul(self.loc_scatterer[indices,cp.newaxis],self.kvector[cp.newaxis,:])
             psi = cp.exp(1j*(r_k.transpose(2,0,1)+phases_rand)).sum(2).transpose(1,0)
             psi2d = psi[:,:,cp.newaxis] * spectrum[cp.newaxis,:]
             mode_int = cp.abs(psi2d)**2
@@ -177,19 +178,16 @@ class Signal():
 
         elif self.alpha_modes == 2:
             num_scatterer = np.array([self.num_photons//3*2, self.num_photons//3])
-            indices = cp.arange(self.num_photons)
-            cp.random.shuffle(indices)
-            indices = (indices[:num_scatterer[0]], indices[-num_scatterer[1]:])
-            alpha_indices = []
+            ind1 = cp.random.randint(self.loc_scatterer.min(), self.loc_scatterer.max(), size=(self.num_modes, num_scatterer[0]//self.num_modes)) % self.ind_scale
+            ind2 = cp.random.randint(self.loc_scatterer.min(), self.loc_scatterer.max(), size=(self.num_modes, num_scatterer[1]//self.num_modes)) % self.ind_scale
+            indices = (ind1, ind2)
             int_tot = cp.zeros(self.det_shape)
-            for i in range(2):
-                alpha_indices.append(cp.array(indices[i][:len(indices[i])//self.num_modes*self.num_modes]).reshape(self.num_modes, len(indices[i])//self.num_modes))
             for k in range(self.alpha_modes):
                 if self.incoherent:
                     phases_rand = cp.array(cp.random.random(size=(self.num_modes, num_scatterer[k]//self.num_modes))*2*cp.pi)
                 else:
                     phases_rand = cp.zeros(self.num_modes, num_scatterer[k]//self.num_modes)
-                r_k = cp.matmul(self.loc_scatterer[alpha_indices[k]%len(self.loc_scatterer),cp.newaxis],self.kvector[cp.newaxis,:])
+                r_k = cp.matmul(self.loc_scatterer[indices[k]%len(self.loc_scatterer),cp.newaxis],self.kvector[cp.newaxis,:])
 
                 psi = cp.exp(1j*(r_k.transpose(2,0,1)+phases_rand)).sum(2).transpose(1,0)
                 psi2d = psi[:,:,cp.newaxis] * klist[k][cp.newaxis,:]