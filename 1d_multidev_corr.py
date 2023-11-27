correct normalization of g2, zscore calculation

1d_multidev_corr.py
@@ -36,7 +36,6 @@ class ProcessCorr():
         self.output_fname = output_fname
         self.np_dark = None
         self.corr = None
-        self.cross_corr = None
         self.integ = None
         self.isums = None
         self.isums_med = None
@@ -55,7 +54,6 @@ class ProcessCorr():
         self.cudark = cp.array(self.np_dark)
         self.cumask = cp.array(self.np_mask)
         self.corr = cp.zeros((self.num_bins, self.fshape[0], self.fshape[1]))
-        self.cross_corr = cp.zeros((self.num_bins, self.fshape[0], self.fshape[1]))
         self.integ = cp.zeros((self.num_bins, self.fshape[0], self.fshape[1]))
         self.corrsq = cp.zeros_like(self.corr)
         self.bin_hist = cp.zeros(self.num_bins)
@@ -87,14 +85,13 @@ class ProcessCorr():
             pass
 
         corr_arr = mp.Array(ctypes.c_double, num_jobs*self.num_bins*self.fshape[0]*self.fshape[1])
-        cross_corr_arr = mp.Array(ctypes.c_double, num_jobs*self.num_bins*self.fshape[0]*self.fshape[1])
         integ_arr = mp.Array(ctypes.c_double, num_jobs*self.num_bins*int(np.product(self.fshape)))
         corrsq_arr = mp.Array(ctypes.c_double, num_jobs*self.num_bins*self.fshape[0]*self.fshape[1])
         bin_hist_arr = mp.Array(ctypes.c_double, num_jobs*self.num_bins)
         isums_arr = mp.Array(ctypes.c_double, 1000*len(self.flist))
         jobs = [mp.Process(target=self._mp_worker, args=(d, self.flist,
                                                          corr_arr,
-                                                         cross_corr_arr, integ_arr, corrsq_arr, isums_arr, 
+                                                         integ_arr, corrsq_arr, isums_arr, 
                                                          bin_hist_arr,
                                                          kwargs['adu_thresh'],
                                                          kwargs['norm']))
@@ -105,16 +102,14 @@ class ProcessCorr():
         self.isums = np.frombuffer(isums_arr.get_obj())
         nframes = self.isums.shape[0]
         self.corr = np.frombuffer(corr_arr.get_obj()).reshape((num_jobs, self.num_bins) + (self.fshape[0], self.fshape[1]))
-        self.cross_corr = np.frombuffer(cross_corr_arr.get_obj()).reshape((num_jobs, self.num_bins) + (self.fshape[0], self.fshape[1]))
         self.corr = self.corr.sum(0) 
-        self.cross_corr = self.cross_corr.sum(0) 
         self.integ = np.frombuffer(integ_arr.get_obj()).reshape((num_jobs, self.num_bins) + self.fshape)
         self.integ = self.integ.sum(0)
         self.corrsq = np.frombuffer(corrsq_arr.get_obj()).reshape((num_jobs,) + self.corr.shape)
         self.corrsq = self.corrsq.sum(0) 
         self.bin_hist = np.frombuffer(bin_hist_arr.get_obj()).reshape((num_jobs, self.num_bins)).sum(0)
 
-    def _mp_worker(self, rank, flist, corr_arr, cross_corr_arr, integ_arr, corrsq_arr, isums_arr, bin_hist, adu_thresh, norm):
+    def _mp_worker(self, rank, flist, corr_arr, integ_arr, corrsq_arr, isums_arr, bin_hist, adu_thresh, norm):
         num_jobs = NUM_DEV * JOBS_PER_DEV
         devnum = rank // JOBS_PER_DEV
         cp.cuda.Device(devnum).use()
@@ -124,7 +119,6 @@ class ProcessCorr():
         self._init_corr()
         kwargs = {'adu_thresh': adu_thresh, 'norm': norm}
         mycorr = np.frombuffer(corr_arr.get_obj()).reshape((num_jobs, self.num_bins) + (self.fshape[0], self.fshape[1]))
-        mycrosscorr = np.frombuffer(cross_corr_arr.get_obj()).reshape((num_jobs, self.num_bins) + (self.fshape[0], self.fshape[1]))
         myinteg = np.frombuffer(integ_arr.get_obj()).reshape((num_jobs,self.num_bins) + self.fshape)
         mycorrsq = np.frombuffer(corrsq_arr.get_obj()).reshape((num_jobs,) + mycorr.shape[1:])
     
@@ -138,7 +132,6 @@ class ProcessCorr():
             if rank == 0:
                 sys.stderr.write(',  %.3f s/file\n' % ((time.time()-stime) / (i+1)))
         mycorr[rank] += self.corr.get()
-        mycrosscorr[rank] += self.cross_corr.get()
         myinteg[rank] += self.integ.get()
         mycorrsq[rank] += self.corrsq.get()
         mybin_hist[rank] += self.bin_hist.get()
@@ -169,30 +162,12 @@ class ProcessCorr():
             else:
                 bin_idx = cp.argmin(cp.abs(cp.array(self.bin_boundaries) - isum_tmp))
 
-
-            if n_tot % 1000 == 0: 
-                sfr_tmp[bin_idx] = cp.copy(fr_integ)
-                isums[n_tot] = 0
-                n += 1
-                n_tot += 1
-                continue
-
-            if self.min_val is not None and self.max_val is not None:
-                if float(fr_integ.sum()) < self.min_val or float(fr_integ.sum()) > self.max_val:
-                    sfr_tmp[bin_idx] = cp.copy(fr_integ)
-                    isums[n_tot] = 0
-                    n += 1
-                    n_tot += 1
-                    continue
-
             isums[n_tot] = isum_tmp 
             sfr_corr_tmp = cusignal.fftconvolve(sfr_tmp[bin_idx], fr_integ[::-1,:], mode='same', axes=0)
             sfr_tmp[bin_idx] = cp.copy(fr_integ)
 
-
             self.bin_hist[bin_idx] += 1
             self.corr[bin_idx] += fr_corr
-            self.cross_corr[bin_idx] += sfr_corr_tmp
             self.integ[bin_idx] += fr_integ
             self.corrsq[bin_idx] += cp.square(fr_corr)
             if fnum % num_jobs == 0:
@@ -206,11 +181,8 @@ class ProcessCorr():
         npix = self.fshape[0] * self.fshape[1]
         bsize = npix // 32 + 1
         self.k_thresh_frame((bsize,), (32,), (sfr, npix, adu_thresh, self.cumask))
-        #sfr[sfr<adu_thresh] = 0
-        #cp.multiply(sfr, self.cumask, out=sfr)
         if norm:
             cp.divide(sfr, cundimage.maximum_filter(sfr, 3, mode='constant'), out=sfr)
-            #cp.divide(sfr, 9*cundimage.uniform_filter(sfr, 3, mode='constant'), out=sfr)
             sfr[cp.isnan(sfr) | cp.isinf(sfr)] = 0
         return cusignal.fftconvolve(sfr, sfr[::-1,:], mode='same', axes=0), sfr
 
@@ -218,20 +190,13 @@ class ProcessCorr():
         if self.num_bins > 1:
             integ = self.integ*self.np_mask
             integ_corr = signal.fftconvolve(integ, integ[:,::-1,:], mode='same', axes=1) 
-            #ncorr = ((self.corr - 1/2 * (self.cross_corr + self.cross_corr[:,::-1,::-1]))/ integ_corr).T * self.bin_hist
             ncorr = (self.corr/ integ_corr).T * self.bin_hist
             ncorr[np.isnan(ncorr) | np.isinf(ncorr)] = 1
             weights = integ.mean((1,2))**2 * self.bin_hist
             wncorr = (ncorr * weights).T.sum(0) / weights.sum()
             ncorr = wncorr
         else:
-            #ncorr = (self.corr[0] - 1/2 * (self.cross_corr[0] + self.cross_corr[0,::-1,:])) / signal.fftconvolve(self.integ[0], self.integ[0,::-1,:], axes=0) 
             ncorr = self.corr[0] / signal.fftconvolve(self.integ[0], self.integ[0][::-1,:], mode='same', axes=0) 
-            
-        #s = int(self.fshape[0]*0.4), int(self.fshape[1]*0.2)
-        #e = int(self.fshape[0]*0.6)+1, int(self.fshape[1]*0.8)+1
-        #ncorr /= np.median(ncorr[s[0]:e[0], s[1]:e[1]])
-        #ncorr /= np.abs(ncorr[269+1, 319+1])
         return ncorr
 
     def save_corr(self, idx=None):
@@ -243,18 +208,19 @@ class ProcessCorr():
         if idx is not None:
             self.output_fname = self.output_fname[:-3] + '_' + str(idx*self.frames_per_file) + '.h5'
         
+        self.corr /= len(self.isums)
+        self.integ /= len(self.isums)
+        self.corrsq /= len(self.isums)
         print('Writing output to', self.output_fname)
         with h5py.File(self.output_fname, 'w') as f:
             if self.num_bins > 1:
-                f['cross_corr_numr'] = self.cross_corr / len(self.isums)
-                f['integ_frame'] = self.integ / len(self.isums)
-                f['corr_numr'] = self.corr / len(self.isums)
-                f['corrsq_numr'] = self.corrsq / len(self.isums)
+                f['integ_frame'] = self.integ
+                f['corr_numr'] = self.corr 
+                f['corrsq_numr'] = self.corrsq
             else:
-                f['cross_corr_numr'] = self.cross_corr[0] / len(self.isums)
-                f['corr_numr'] = self.corr[0] / len(self.isums)
-                f['integ_frame'] = self.integ[0] / len(self.isums)
-                f['corrsq_numr'] = self.corrsq[0] / len(self.isums)
+                f['corr_numr'] = self.corr[0] 
+                f['integ_frame'] = self.integ[0]
+                f['corrsq_numr'] = self.corrsq[0]
             f['frame_sums'] = self.isums
             f['normalized_corr'] = self.norm_corr()
             f['goodpix_mask'] = self.np_mask
@@ -280,7 +246,6 @@ def main():
         runs = [int(r) for r in config.get(section, 'runs').split()]
     else:
         runs = [args.run_num]
-    #start = config.getint(section, 'start_block', fallback=0)
     starts = [int(s) for s in config.get(section, 'start_block').split()]
     file_chunk = config.getint(section, 'file_chunk', fallback=1000)
     end_orig = config.getint(section, 'stop_block', fallback=None)
@@ -292,14 +257,12 @@ def main():
     max_val = config.getint(section, 'max_val', fallback=None)
     suffix = config.get(section, 'output_suffix', fallback=None)
 
-
-
     for r in runs:
         for s in starts:
             print('Processing run %s:%d' % (exp, r))
             data_glob = '/mpsd/cni/processed/wittetam/sim/raw/%s/Run%d_*.npy' % (exp, r)
             output_fname = 'data/%s_Run%d' % (exp,r)
-            output_fname += '.h5'
+            output_fname += '_1d.h5'
             print('Writing output to', output_fname)
 
             flist = natsort.natsorted(glob.glob(data_glob))

kalpha.py
@@ -26,12 +26,12 @@ from cupyx.scipy import signal as cusignal
 from cupyx.scipy import ndimage as cundimage
 plt.ion()
 
-NUM_DEV = 1 
+NUM_DEV = 1
 JOBS_PER_DEV = 1
 
 class Signal():  
     def __init__(self, elements, emission_lines, det_shape=(1024,1024), binning=8, num_shots=1000, num_photons=50,
-                 noise=60, incoherent=False, efilter=False, alpha_modes=2, det_dist=4, pixel_size=100):
+                 noise=60, incoherent=False, efilter=False, alpha_modes=2, det_dist=4, pixel_size=100, fov=20):
 
         self.elements = elements
         self.lines = emission_lines
@@ -49,7 +49,7 @@ class Signal():
         self.num_photons = num_photons
 
         self.size_em = 5
-        self.sample_shape = (20,20)
+        self.sample_shape = (fov, fov)
         self.sample = None
         self.hits = []
         self.hit_size = None
@@ -301,6 +301,7 @@ if __name__ == '__main__':
     det_dist = config.getfloat(section, 'det_dist', fallback=1.)
     print(det_dist)
     pixel_size = config.getint(section, 'pixel_size', fallback=100)
+    fov = config.getint(section, 'fov', fallback=20)
     det_shape = fshape
     #num_photons = np.ceil(args.photon_density * det_shape[0] * det_shape[1]).astype(int)
 
@@ -311,7 +312,7 @@ if __name__ == '__main__':
     print('Simulate {} line for {}'.format(emission_lines, elements))
  
     sig = Signal(elements, emission_lines, det_shape=det_shape, binning=binning, num_shots=num_shots, num_photons=num_photons, 
-                 noise=noise, incoherent=incoherent, efilter=efilter, alpha_modes=alpha, det_dist=det_dist, pixel_size=pixel_size)
+                 noise=noise, incoherent=incoherent, efilter=efilter, alpha_modes=alpha, det_dist=det_dist, pixel_size=pixel_size, fov=fov)
     sig.create_sample()
     sig.sim_glob()
 

kbeta.py
@@ -31,7 +31,7 @@ JOBS_PER_DEV = 1
 
 class Signal():  
     def __init__(self, elements, lines, det_shape=(1024,1024), binning=8, num_shots=1000, num_photons=50,
-                 emission_line='kb1', noise=60, efilter=False, det_dist=4, pixel_size=100):
+                 emission_line='kb1', noise=60, efilter=False, det_dist=4, pixel_size=100, fov=10):
 
         self.elements = elements
         self.lines = lines
@@ -49,10 +49,7 @@ class Signal():
 
         self.size_em1 = 5
         self.size_em2 = 7
-        if self.det_distance == 3:
-            self.sample_shape = (160,160)
-        else:
-            self.sample_shape = (64,64)
+        self.sample_shape = (fov,fov)
         #self.size_np = 100e-9 #particle size in m
         self.sample = None
         self.hits = []
@@ -306,6 +303,7 @@ if __name__ == '__main__':
     efilter = config.getboolean(section, 'filter', fallback=True)
     det_dist = float(config.get(section, 'det_dist', fallback=1))
     pixel_size = config.getint(section, 'pixel_size', fallback=100)
+    fov = config.getint(section, 'fov', fallback=160)
     line = config.get(section, 'emission_line', fallback='kb1')
     det_shape = fshape
     #num_photons = np.ceil(args.photon_density * det_shape[0] * det_shape[1]).astype(int)
@@ -316,7 +314,7 @@ if __name__ == '__main__':
     print('Detector distance: ', det_dist)
     print('Simulate {} line for {}'.format(emission_lines, elements))
  
-    sig = Signal(elements, emission_lines, det_shape=det_shape, binning=binning, num_shots=num_shots, num_photons=num_photons, emission_line=line, noise=noise, efilter=efilter, det_dist=det_dist, pixel_size=pixel_size)
+    sig = Signal(elements, emission_lines, det_shape=det_shape, binning=binning, num_shots=num_shots, num_photons=num_photons, emission_line=line, noise=noise, efilter=efilter, det_dist=det_dist, pixel_size=pixel_size, fov=fov)
     sig.create_sample()
     sig.sim_glob()
 